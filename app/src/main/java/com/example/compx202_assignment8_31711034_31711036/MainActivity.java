7a7e9aca9ce4c6efda3f2c0b7be24c68147ee373
scdow <920301714@qq.com>
2020-06-17 15:56:12 +0800

    Empty application

---
+++ app/src/main/java/com/example/compx202_assignment8_31711034_31711036/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.compx202_assignment8_31711034_31711036;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/example/compx202_assignment8_31711034_31711036/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.compx202_assignment8_31711034_31711036;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
