ed532d876b6fe9f38797edbf5e804cc5cb3b8262
PeijiaShi <1120069786@qq.com>
2020-06-18 21:12:13 +0800

    Connect main interface and game interface successfully 1. activity_main.xml: add game name & add developer 2. activity_main.xml: create a Start button 3. colors.xml & strings.xml: change the color and add the string 4. Button_style.xml: give the button a style 5. MainActivity.java & GameActivity.java: hide the active bar and set stick immersive full-screen mode 6. MainActivity,java: connect the main interface and game interface

--- app/src/main/java/com/example/compx202_assignment8_31711034_31711036/GameActivity.java
+++ app/src/main/java/com/example/compx202_assignment8_31711034_31711036/GameActivity.java
@@ -1,13 +1,23 @@
 package com.example.compx202_assignment8_31711034_31711036;
 
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //hide the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //set stick immersive full-screen mode
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
     }
 }
 
--- app/src/main/java/com/example/compx202_assignment8_31711034_31711036/MainActivity.java
+++ app/src/main/java/com/example/compx202_assignment8_31711034_31711036/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.compx202_assignment8_31711034_31711036;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,16 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hide the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //set stick immersive full-screen mode
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+
     }
 
-    public void onclickStart (View view){
-
-//        Toast.makeText(this, "onclickStart work", Toast.LENGTH_SHORT).show();
-
-        // Create new intent for ListActivity
+    public void onClickButtonStart(View view){
+//        Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, GameActivity.class);
-
-        // Start the activity with the intent
         startActivity(intent);
 
     }
