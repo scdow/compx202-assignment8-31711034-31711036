436040fc7650fa08599030468a0558d96d50b934
scdow <920301714@qq.com>
2020-06-18 15:05:16 +0800

    half connection of home page and game page

---
+++ app/src/main/java/com/example/compx202_assignment8_31711034_31711036/GameActivity.java
@@ -0,0 +1,14 @@
+package com.example.compx202_assignment8_31711034_31711036;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
+
+
--- app/src/main/java/com/example/compx202_assignment8_31711034_31711036/MainActivity.java
+++ app/src/main/java/com/example/compx202_assignment8_31711034_31711036/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.compx202_assignment8_31711034_31711036;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onclickStart (View view){
+
+//        Toast.makeText(this, "onclickStart work", Toast.LENGTH_SHORT).show();
+
+        // Create new intent for ListActivity
+        Intent intent = new Intent(this, GameActivity.class);
+
+        // Start the activity with the intent
+        startActivity(intent);
+
     }
 }
